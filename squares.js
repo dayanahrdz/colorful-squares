631325456deb0a9c5461eab733a29c57a82d6a8c
Dayana Hernandez <dayanahern15@gmail.com>
2025-11-19 09:25:31 -0500

    class retry

---
+++ squares.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+    var add= document.getElementById("add");
+    add.onclick = addSquare;
+    var squareCount = parseInt(Math.random() * 1000) + 30;
+    for(let i = 0; i < squareCount; i++){
+        addSquare();
+    }
+    
+}
+function getRandomColor(){
+    var possibilities = "0123456789ABCDEF";
+    var result="#";
+    for(var i=0; i <6; i++)
+    {
+        result+=possibilities.charAt(parseInt(
+            Math.random()*possibilities.length));
+    }
+    return result;
+}
+
+function addSquare(){
+    var squarearea = document.getElementById("squarearea");
+    var newSquare = document.createElement("div");
+    newSquare.className="square";
+    newSquare.style.left = parseInt(Math.random()* 651) +"px";
+    newSquare.style.top = parseInt(Math.random()* 251) +"px";
+    newSquare.style.backgroundColor = getRandomColor();
+    squarearea.appendChild(newSquare); //append newSquare element will happen at the end
+ 
+}
